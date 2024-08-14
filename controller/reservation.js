1629966ce8c1df7e211844f110a8daff6d564335
mohit0709 <mohitaglawe16@gmail.com>
2024-08-14 21:10:02 +0530

    controller folder added

---
+++ controller/reservation.js
@@ -0,0 +1,27 @@
+import ErrorHandler from "../error/error";
+import {Reservation} from "../models/reservationSchema.js"
+
+export const sendReservation = async (req, res, next) => {
+
+    const {firstName, lastName, email, phone, date, time} = req.body
+    if (!firstName || !lastName || !email || !phone|| !date || !time){
+        return next(new ErrorHandler("Please fill all the reservation details!", 400))
+    }
+
+    try {
+        await Reservation.create(firstName, lastName, email, phone, date, time)
+        res.status(200)
+          json({
+            success: true,
+            message: "Reservation sent successfully!"
+          })
+    }
+    catch(err){
+        if (error.name == "ValidationError") {
+            const validationErrors = Object.values(error.errors).map(
+                (err) => err.message
+            )
+            return next(new ErrorHandler(validationErrors.join(" , "), 400))
+        }
+    }
+}
